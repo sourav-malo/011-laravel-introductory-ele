d0f2e56a43737c0e0328a96fdd22ef6bf5fbcfcd
Sourav Malo <sourav.cse5.bu@gmail.com>
2023-01-15 19:20:57 +0600

    learnt basics of breeze authentication

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,9 +4,9 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Home</title>
+  <title>Dashboard</title>
 </head>
 <body>
-  <h1>This is Home Page!</h1>
+  <h1>This is Dashboard Page!</h1>
 </body>
 </html>
